f1cf12d8f87a64acd415a85339482b8f34171da1
James Walker <james.chunho@gmail.com>
2026-02-09 12:38:34 +0800

    improve quoting logic

--- src/serialize.rs
+++ src/serialize.rs
@@ -1,48 +1,84 @@
 use crate::parser::{Child, Element};
-use std::borrow::Cow;
+use std::{borrow::Cow, iter::once};
 
-fn serialise_term(text: &str) -> Cow<str> {
+const QUOTE_CHARS: [char; 3] = ['"', '\'', '`'];
+
+struct TermInfo {
+    must_quote: bool,
+    has_double_quote: bool,
+    has_single_quote: bool,
+    has_backtick_quote: bool,
+}
+
+fn gather_term_info(text: &str) -> TermInfo {
+    // conditions for needing to quote a term:
+    // - it starts with a quote char, or
+    // - it contains a space
+
+    // if it starts with any quote char, then it will always be quoted
+    let mut must_quote = text.starts_with(&QUOTE_CHARS);
+    let mut has_double_quote = false;
+    let mut has_single_quote = false;
+    let mut has_backtick_quote = false;
+
+    for x in text.chars() {
+        if x == ' ' {
+            must_quote = true;
+        } else if x == '"' {
+            has_double_quote = true;
+        } else if x == '\'' {
+            has_single_quote = true;
+        } else if x == '`' {
+            has_backtick_quote = true;
+        }
+    }
+
+    TermInfo {
+        must_quote,
+        has_double_quote,
+        has_single_quote,
+        has_backtick_quote,
+    }
+}
+
+fn serialise_term<'a>(text: &'a str) -> Cow<'a, str> {
     if text.is_empty() {
         return "\"\"".into();
     }
 
-    let mut text: Cow<str> = text.into();
+    let info = gather_term_info(text);
 
-    // backticks are not allowed in .rpp files, they are always replaced with single quotes
-    if text.contains('`') {
-        text = text.replace('`', "'").into();
-    }
-
-    let first_char = text.chars().next().unwrap();
-    let mut needs_to_be_quoted = first_char == '\'' || first_char == '"';
-
-    let mut has_dbl_quote = false;
-    let mut has_sgl_quote = false;
-    for x in text.chars() {
-        if x == ' ' {
-            needs_to_be_quoted = true;
-        } else if x == '\'' {
-            has_sgl_quote = true;
-        } else if x == '"' {
-            has_dbl_quote = true;
-        }
-    }
-
-    if !needs_to_be_quoted {
-        return text;
-    }
-
-    let quote_char = {
-        if has_dbl_quote && has_sgl_quote {
-            '`'
-        } else if has_dbl_quote {
-            '\''
-        } else {
+    if info.must_quote {
+        let mut replace_backticks = false;
+        let quote_char = if !info.has_double_quote {
             '"'
-        }
-    };
+        } else if !info.has_single_quote {
+            '\''
+        } else if !info.has_backtick_quote {
+            '`'
+        } else {
+            // Reaper does not allow terms with all 3 quote characters.
+            // Backticks will be replaced with single quotes.
+            replace_backticks = true;
+            '`'
+        };
 
-    format!("{}{}{}", quote_char, text, quote_char).into()
+        let quoted_text: String = if replace_backticks {
+            once(quote_char)
+                .chain(text.chars().map(|x| if x == '`' { '\'' } else { x }))
+                .chain(once(quote_char))
+                .collect()
+        } else {
+            once(quote_char)
+                .chain(text.chars())
+                .chain(once(quote_char))
+                .collect()
+        };
+
+        quoted_text.into()
+    } else {
+        text.into()
+    }
 }
 
 /// Serialise an element back to a [String] following the RPP format.
@@ -100,4 +136,114 @@ fn process(buf: &mut String, element: &Element, indent_level: usize) {
         buf.push_str("  ")
     }
     buf.push('>');
+}
+
+#[cfg(test)]
+mod serialise_term_tests {
+    use super::*;
+
+    #[test]
+    fn test_01() {
+        let input = r#"as d"#;
+        let expected = r#""as d""#;
+        assert_eq!(serialise_term(input), expected);
+    }
+
+    #[test]
+    fn test_02() {
+        let input = r#"'as d'"#;
+        let expected = r#""'as d'""#;
+        assert_eq!(serialise_term(input), expected);
+    }
+
+    #[test]
+    fn test_03() {
+        let input = r#""as d""#;
+        let expected = r#"'"as d"'"#;
+        assert_eq!(serialise_term(input), expected);
+    }
+
+    #[test]
+    fn test_04() {
+        let input = r#"`as d`"#;
+        let expected = r#""`as d`""#;
+        assert_eq!(serialise_term(input), expected);
+    }
+
+    #[test]
+    fn test_05() {
+        let input = r#"'as d"as d'"#;
+        let expected = r#"`'as d"as d'`"#;
+        assert_eq!(serialise_term(input), expected);
+    }
+
+    #[test]
+    fn test_06() {
+        let input = r#""as d`as d""#;
+        let expected = r#"'"as d`as d"'"#;
+        assert_eq!(serialise_term(input), expected);
+    }
+
+    #[test]
+    fn test_07() {
+        let input = r#"`as d'as d`"#;
+        let expected = r#""`as d'as d`""#;
+        assert_eq!(serialise_term(input), expected);
+    }
+
+    #[test]
+    fn test_08() {
+        let input = r#"a'"`b"#;
+        let expected = r#"a'"`b"#;
+        assert_eq!(serialise_term(input), expected);
+    }
+
+    #[test]
+    fn test_09() {
+        let input = r#"a'"`b       a'"`b"#;
+        let expected = r#"`a'"'b       a'"'b`"#;
+        assert_eq!(serialise_term(input), expected);
+    }
+
+    #[test]
+    fn test_10() {
+        let input = r#"'asd"#;
+        let expected = r#""'asd""#;
+        assert_eq!(serialise_term(input), expected);
+    }
+
+    #[test]
+    fn test_11() {
+        let input = r#""asd"#;
+        let expected = r#"'"asd'"#;
+        assert_eq!(serialise_term(input), expected);
+    }
+
+    #[test]
+    fn test_12() {
+        let input = r#"`asd"#;
+        let expected = r#""`asd""#;
+        assert_eq!(serialise_term(input), expected);
+    }
+
+    #[test]
+    fn test_13() {
+        let input = r#"asd'"#;
+        let expected = r#"asd'"#;
+        assert_eq!(serialise_term(input), expected);
+    }
+
+    #[test]
+    fn test_14() {
+        let input = r#"asd`"#;
+        let expected = r#"asd`"#;
+        assert_eq!(serialise_term(input), expected);
+    }
+
+    #[test]
+    fn test_15() {
+        let input = r#"asd""#;
+        let expected = r#"asd""#;
+        assert_eq!(serialise_term(input), expected);
+    }
 }
